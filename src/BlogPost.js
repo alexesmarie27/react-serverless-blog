b82ccdfcf9b80f85b6306f50041b0330f4bd9ceb
Alexes Presswood <apresswood@rtsfinancial.com>
2017-11-04 11:21:42 -0500

    Add ButterCMS to BLogPost

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
+import Butter from 'buttercms'
+
+const butter = Butter('de55d3f93789d4c5c26fb07445b680e8bca843bd');
 
 class BlogPost extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false
+    };
+  }
+
+  componentWillMount() {
+    let slug = this.props.params.slug;
+
+    butter.post.retrieve(slug).then((resp) => {
+      this.setState({
+        loaded: true,
+        post: resp.data.data
+      })
+    });
+  }
+
   render() {
-    return (
-      <div>
-        Post
-      </div>
-    );
+    if (this.state.loaded) {
+      const post = this.state.post;
+
+      return (
+        <div>
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.body}} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }
   }
 }
 
 export default BlogPost;
-
