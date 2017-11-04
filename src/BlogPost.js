b92215d05a0830aae0afd0daaca5151d033cc98a
Alexes Presswood <apresswood@rtsfinancial.com>
2017-11-04 11:11:47 -0500

    Add BlogHome and BlogPost

---
+++ src/BlogPost.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BlogPost extends Component {
+  render() {
+    return (
+      <div>
+        Post
+      </div>
+    );
+  }
+}
+
+export default BlogPost;
+
